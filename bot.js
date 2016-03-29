61778d8a7531546a33e84075eb9a2140eee0b8a5
scrowe34 <seancrowe35@gmail.com>
2016-03-28 20:06:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond()
 {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/drivers/;  botRegexDL = /^\/Cleveland/i;botRegexSalt = /^\/jake/;botRegexRules = /^\/fuf/
-      botRegexAd=/^\/credit card rewards/;botRegexGTA = /^\/ram/; botRegexSC = /^\/sup jake/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
+      botRegexAd=/^\/credit card rewards/;botRegexGTA = /^\/theme/; botRegexSC = /^\/sup jake/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/a/i;  botRegexTw = /^\/Jake/i; botRegexSb = /^\/Northbrook/; botRegexSh = /^\/help/; botRegexWk = /^\/users/; botRegexCC = /^\/snail/;
       botRegexSiege = /^\/siege/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
@@ -49,8 +49,8 @@ function respond()
   else if(request.text && botRegexGTA.test(request.text))
   {
     this.res.writeHead(200);
-    postMessage("http://i.imgur.com/DWubPb3.jpg");
-
+   
+postMessage("pajamas");
 
     this.res.end();
   } 
