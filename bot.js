af1d61f46b174fe1dfbfebfb3ca820c7e9751e92
scrowe34 <seancrowe35@gmail.com>
2016-03-30 12:09:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond()
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/drivers/;  botRegexDL = /^\/Cleveland/i;botRegexSalt = /^\/jake/;botRegexRules = /^\/fuf/
       botRegexAd=/^\/credit card rewards/;botRegexGTA = /^\/theme/; botRegexSC = /^\/sup jake/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
-      botRegexP = /^\/a/i;  botRegexTw = /^\/Jake/i; botRegexSb = /^\/Northbrook/; botRegexSh = /^\/help/; botRegexWk = /^\/users/; botRegexCC = /^\/snail/;
+      botRegexP = /^\/a/i;  botRegexTw = /^\/Jake/i; botRegexSb = /^\/Northbrook/; botRegexSh = /^\/wolf/; botRegexWk = /^\/users/; botRegexCC = /^\/snail/;
       botRegexSiege = /^\/siege/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -80,7 +80,7 @@ postMessage("pajamas");
   else if(request.text && botRegexSh.test(request.text)) 
   {
     this.res.writeHead(200);
-    postMessage("Look it up fuck cake");
+    postMessage("http://i.imgur.com/4Ae1GPz.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexWk.test(request.text)) 
