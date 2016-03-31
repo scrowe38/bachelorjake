c513fe431a1bb02dc0fbd11d0ef6fcf4477fd1f2
scrowe34 <seancrowe35@gmail.com>
2016-03-31 18:17:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond()
       botRegex = /^\/gmail/;  botRegexDL = /^\/Cleveland/i;botRegexSalt = /^\/jake/;botRegexRules = /^\/fuf/
       botRegexAd=/^\/credit card rewards/;botRegexGTA = /^\/theme/; botRegexSC = /^\/sup jake/i; botODB = /\/Yo/i; botDuck = /^\/duck/;
       botRegexP = /^\/bowser/i;  botRegexTw = /^\/Jake/i; botRegexSb = /^\/Northbrook/; botRegexSh = /^\/wolf/; botRegexWk = /^\/users/; botRegexCC = /^\/snail/;
-      botRegexSiege = /^\/I miss her/;
+      botRegexSiege = /^\/♋️/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -110,7 +110,7 @@ postMessage("pajamas");
   else if(request.text && botRegexSiege.test(request.text)) 
   {
     this.res.writeHead(200);
-   
+   postMessage("http://i.imgur.com/yXh1wEo.jpg");
     this.res.end();
   }
   
